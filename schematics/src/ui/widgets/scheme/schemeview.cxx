635d70afa501c19cb9a680f293c7b4f6a45295b5
Andreevo LDK <andreevo.ldk@yandex.ru>
2020-09-15 14:42:08 +0300

    add visual geometry calculation

--- schematics/src/ui/widgets/scheme/schemeview.cxx
+++ schematics/src/ui/widgets/scheme/schemeview.cxx
@@ -34,6 +34,7 @@ namespace Schematics::Ui::Widgets
                 // swap sizes
                 auto r = rect();
                 setRect(r.x(), r.y(), r.height(), r.width());
+                updatePos();
             }
         }
         bool isVertical() const { return m_orientation == Vertical; }
@@ -46,6 +47,59 @@ namespace Schematics::Ui::Widgets
             }
             auto r = rect();
             setRect(r.x(), r.y(), w, h);
+            updatePos();
+        }
+        void setWidth(double width)
+        {
+            auto r = rect();
+
+            switch (orientation()) {
+            case Vertical:
+                r.setHeight(width);
+                break;
+            case Horisontal:
+                r.setWidth(width);
+                break;
+            }
+            setRect(r);
+            updatePos();
+        }
+        void setHeight(double height)
+        {
+            auto r = rect();
+
+            switch (orientation()) {
+            case Vertical:
+                r.setWidth(height);
+                break;
+            case Horisontal:
+                r.setHeight(height);
+                break;
+            }
+            setRect(r);
+            updatePos();
+        }
+        void updatePos()
+        {
+            auto r = rect();
+
+            switch (orientation()) {
+            case Vertical:
+                r.moveTo(r.x(), - r.height() / 2);
+                break;
+            case Horisontal:
+                {
+                    auto w = r.width();
+                    r.setX(- w / 2);
+                    r.setWidth(w);
+                }
+                break;
+            }
+            setToolTip(QString{"%1 x %2"}
+                           .arg(r.width(), 0, 'f', 1)
+                           .arg(r.height(), 0, 'f', 1)
+                       );
+            setRect(r);
         }
         void setFillColor(const QColor& color)
         {
@@ -90,9 +144,30 @@ namespace Schematics::Ui::Widgets
 
     struct SchemeItems
     {
+        enum Side
+        {
+            LeftSide, RightSide,
+            TopSide = LeftSide,
+            BottomSide = RightSide
+        };
+        using CentralBoards = QList<DWS350BoardItem*>;
+
+        CentralBoards dws350;
+        PA300BoardItem* pa300[2];
+        PKA350BoardItem* pka350[2];
+        PA350BoardItem* pa350[2];
+
         QGraphicsEllipseItem* diam = nullptr;
         QGraphicsLineItem* x_axis = nullptr;
         QGraphicsLineItem* y_axis = nullptr;
+
+        bool pkaIsVertical = false;
+
+        double dwsSaw() const
+        { return 4.8; }
+
+        double pkaSaw() const
+        { return 5.6; }
 
         static SchemeItems* create(SchemeScene* gfx)
         {
@@ -103,39 +178,288 @@ namespace Schematics::Ui::Widgets
 
         SchemeItems* build(SchemeScene* gfx)
         {
-            diam = gfx->addEllipse(-1, -1, 1, 1);
+            // diameter visualisation
             {
+                diam = gfx->addEllipse(-1, -1, 1, 1);
                 auto pen = QPen{Qt::DashLine};
                 pen.setWidth(5);
                 diam->setPen(pen);
                 auto bg = QBrush{Qt::yellow, Qt::BDiagPattern};
                 diam->setBrush(bg);
             }
-            auto axis = QPen{};
-            axis.setStyle(Qt::DashDotLine);
-            axis.setColor(Qt::red);
-            x_axis = gfx->addLine(-1, 0, 1, 0, axis);
-            y_axis = gfx->addLine(0, -1, 0, 1, axis);
+
+            // coord axis visualisation
+            {
+                auto axis = QPen{};
+                axis.setStyle(Qt::DashDotLine);
+                axis.setColor(Qt::red);
+                x_axis = gfx->addLine(-1, 0, 1, 0, axis);
+                y_axis = gfx->addLine(0, -1, 0, 1, axis);
+            }
+
+            for(auto& b: pa300)
+            {
+                b = new PA300BoardItem{};
+                gfx->addItem(b);
+            }
+
+            for(auto& b: pka350)
+            {
+                b = new PKA350BoardItem{};
+                gfx->addItem(b);
+            }
+
+            for(auto& b: pa350)
+            {
+                b = new PA350BoardItem{};
+                gfx->addItem(b);
+            }
 
             return this;
         }
 
         void setDiameter(double diameter)
         {
-            auto x2 = diameter * 2;
-            auto axis = diameter * 1.2;
-            diam->setRect(-diameter, -diameter, x2, x2);
+            auto radius = diameter / 2;
+            auto axis = radius * 1.2;
+            diam->setRect(-radius, -radius, diameter, diameter);
             x_axis->setLine(-axis, 0, axis, 0);
             y_axis->setLine(0, -axis, 0, axis);
+        }
+
+        void setCentralWidth(double width)
+        {
+            for(auto b: dws350)
+            {
+                b->setWidth(width);
+            }
+            updateGeometry();
+        }
+
+        auto addCentral(SchemeScene* gfx, double width, double height)
+        {
+            auto b = new DWS350BoardItem{};
+            b->setHeight(height);
+            gfx->addItem(b);
+            dws350.push_back(b);
+            setCentralWidth(width);
+
+            return b;
+        }
+
+        void setPA300Size(double width, double height)
+        {
+            setDevSize(pa300, width, height);
+        }
+
+        void setPA300Visible(bool visible)
+        {
+            setDevVisible(pa300, visible);
+        }
+
+        bool pa300Visible()
+        { return devIsVisible(pa300); }
+
+        void setPKA350Size(double width, double height)
+        {
+            setDevSize(pka350, width, height);
+        }
+
+        void setPKA350Visible(bool visible)
+        {
+            setDevVisible(pka350, visible);
+        }
+
+        bool pka350Visible()
+        { return devIsVisible(pka350); }
+
+        void setPA350Size(double width, double height)
+        {
+            setDevSize(pa350, width, height);
+        }
+
+        void setPA350Visible(bool visible)
+        {
+            setDevVisible(pa350, visible);
+        }
+
+        bool pa350Visible()
+        { return devIsVisible(pa350); }
+
+        void setVertical(bool isVertical)
+        {
+            pkaIsVertical = isVertical;
+            auto o = isVertical ? BoardItem::Vertical : BoardItem::Horisontal;
+            pka350[LeftSide]->setOrientation(o);
+            pka350[RightSide]->setOrientation(o);
+            pa350[LeftSide]->setOrientation(o);
+            pa350[RightSide]->setOrientation(o);
+            updateGeometry();
+        }
+
+        void updateGeometry()
+        {
+            // calc dws rect
+            auto dw = 0.0;
+            auto dh = dws350.count() == 0 ? 0.0 : dws350[0]->rect().height();
+
+            for(auto b: dws350)
+            {
+                dw += b->rect().width() + dwsSaw();
+            }
+            if (dws350.count() > 0)
+            {
+                dw -= dwsSaw();
+            }
+            auto x_half = dw / 2;
+            auto y_half = dh / 2;
+
+            auto x = -x_half;
+            for(auto b: dws350)
+            {
+                auto r = b->rect();
+
+                b->setRect(x, -y_half, r.width(), r.height());
+
+                x += r.width() + dwsSaw();
+            }
+
+            // update PA300
+            {
+                auto rr = pa300[RightSide]->rect();
+                rr.moveTo(
+                    x_half + dwsSaw(),
+                    -rr.height() / 2.0);
+                pa300[RightSide]->setRect(rr);
+
+                auto rl = pa300[LeftSide]->rect();
+                rl.moveTo(
+                    -x_half - rl.width() - dwsSaw(),
+                    -rl.height() / 2.0);
+                pa300[LeftSide]->setRect(rl);
+            }
+
+            // update PKA350/PA350
+            if (pkaIsVertical)
+            {
+                auto x_offset = x_half + pkaSaw();
+                if (pa300Visible())
+                {
+                    x_offset += dwsSaw() + pa300[RightSide]->rect().width();
+                }
+
+                {
+                    auto rr = pka350[RightSide]->rect();
+                    rr.moveTo(x_offset, - rr.height() / 2.0);
+                    pka350[RightSide]->setRect(rr);
+
+                    auto rl = pka350[LeftSide]->rect();
+                    rl.moveTo(-x_offset - rl.width()
+                              ,- rl.height() / 2.0);
+                    pka350[LeftSide]->setRect(rl);
+                }
+
+                {
+                    auto l_offset = -x_offset;
+                    auto r_offset = x_offset;
+                    if (pka350Visible())
+                    {
+                        l_offset -= pkaSaw() + pka350[LeftSide]->rect().width();
+                        r_offset += pkaSaw() + pka350[RightSide]->rect().width();
+                    }
+
+                    auto rr = pa350[RightSide]->rect();
+                    rr.moveTo(r_offset, - rr.height() / 2.0);
+                    pa350[RightSide]->setRect(rr);
+
+                    auto rl = pa350[LeftSide]->rect();
+                    rl.moveTo(l_offset - rl.width(), - rl.height() / 2.0);
+                    pa350[LeftSide]->setRect(rl);
+                }
+            }
+            else
+            {
+                auto y_offset = y_half + pkaSaw();
+                // pka
+                {
+                    auto rt = pka350[TopSide]->rect();
+                    rt.moveTo(-rt.width() / 2.0, -y_offset - rt.height());
+                    pka350[TopSide]->setRect(rt);
+
+                    auto rb = pka350[BottomSide]->rect();
+                    rb.moveTo(-rb.width() / 2.0, y_offset);
+                    pka350[BottomSide]->setRect(rb);
+                }
+
+                // pa
+                {
+                    auto y_t = - y_offset;
+                    auto y_b =  y_offset;
+
+                    if (pka350Visible())
+                    {
+                        y_t -= pkaSaw() + pka350[TopSide]->rect().height();
+                        y_b += pkaSaw() + pka350[BottomSide]->rect().height();
+                    }
+
+                    auto rt = pa350[TopSide]->rect();
+                    rt.moveTo(-rt.width()/2.0, y_t - rt.height());
+                    pa350[TopSide]->setRect(rt);
+
+                    auto rb = pa350[BottomSide]->rect();
+                    rb.moveTo(-rb.width() / 2.0, y_b);
+                    pa350[BottomSide]->setRect(rb);
+                }
+            }
+        }
+    private:
+        template<class Name>
+        void setDevSize(Name* dev[2], double width, double height)
+        {
+            static_assert(std::is_base_of_v<BoardItem, Name>, "Invalid dev parameter");
+            dev[LeftSide]->setSize(width, height);
+            dev[RightSide]->setSize(width, height);
+            updateGeometry();
+        }
+
+        template<class Name>
+        void setDevVisible(Name* dev[2], bool visible)
+        {
+            static_assert(std::is_base_of_v<BoardItem, Name>, "Invalid dev parameter");
+            dev[LeftSide]->setVisible(visible);
+            dev[RightSide]->setVisible(visible);
+            updateGeometry();
+        }
+
+        template<class Name>
+        bool devIsVisible(Name* dev[2])
+        {
+            static_assert(std::is_base_of_v<BoardItem, Name>, "Invalid dev parameter");
+
+            return dev[LeftSide]->isVisible() && dev[RightSide]->isVisible();
         }
     };
 
     SchemeView::SchemeView(QWidget *parent) : QGraphicsView(parent) {
         gfx = new SchemeScene{this};
         setScene(gfx);
+        scale(2.0, 2.0);
         gfx->setBackgroundBrush(Qt::white);
         scheme = SchemeItems::create(gfx);
         setDiameter(200.0);
+
+//        for(auto sz: {35., 60., 35.})
+//        {
+//            scheme->addCentral(gfx, 300.0, sz);
+//        }
+//        scheme->setPA300Size(250., 35.);
+//        scheme->setPA300Visible(true);
+//        scheme->setPKA350Size(200, 25);
+//        //scheme->setPKA350Visible(true);
+//        scheme->setPA350Size(150, 40);
+//        scheme->setPA350Visible(true);
+//        scheme->setVertical(true);
+//        setDiameter(350.0);
     }
 
     void SchemeView::setDiameter(double diameter)
@@ -145,7 +469,7 @@ namespace Schematics::Ui::Widgets
 
     void SchemeView::setCentralWidth(double width)
     {
-        //
+        scheme->setCentralWidth(width);
     }
 
     SchemeView::~SchemeView()
