56e6eb1cb8fd114b58b4261c3f7c5c167c839b15
Max Dark <maxim.dark@gmail.com>
2020-09-21 23:46:33 +0300

    add base code structure for xmlreader

--- schematics/lib/libschema/schema/xmlreader.cxx
+++ schematics/lib/libschema/schema/xmlreader.cxx
@@ -25,20 +25,80 @@ namespace libschema
             return Unit::from_units(num);
         }
 
+        void do_read_sizes(XmlStream& xml, Unit& width, Unit& height, bool& ok)
+        {
+            while (xml.readNextStartElement())
+            {
+                if (xml.name() == tags::widthTag())
+                {
+                    width = do_read_units(xml, ok);
+                }
+                else if (xml.name() == tags::heightTag())
+                {
+                    height = do_read_units(xml, ok);
+                }
+                else
+                {
+                    xml.skipCurrentElement();
+                }
+            }
+        }
+
         void do_read_scheme(Schema* schema, XmlStream& xml)
         {
-            ;
+            bool ok;
+            Unit tmp_width, tmp_height;
+            while (xml.readNextStartElement())
+            {
+                qDebug() << xml.name();
+                if (xml.name() == tags::dws350Tag())
+                {
+                    while (xml.readNextStartElement())
+                    {
+                        if (xml.name() == tags::widthTag())
+                        {
+                            auto w = do_read_units(xml, ok);
+                            qDebug() << ok << "width = " << w.to_mm();
+                        }
+                        else if (xml.name() == tags::heightTag())
+                        {
+                            auto h = do_read_units(xml, ok);
+                            qDebug() << ok << "height = " << h.to_mm();
+                        }
+                        else
+                        {
+                            xml.skipCurrentElement();
+                        }
+                    }
+                }
+                else if (xml.name() == tags::pa300Tag())
+                {
+                    do_read_sizes(xml, tmp_width, tmp_height, ok);
+                    qDebug() << "pa300" << tmp_width.to_mm() << tmp_height.to_mm();
+                }
+                else if (xml.name() == tags::pka350Tag())
+                {
+                    do_read_sizes(xml, tmp_width, tmp_height, ok);
+                    qDebug() << "pka350" << tmp_width.to_mm() << tmp_height.to_mm();
+                }
+                else if (xml.name() == tags::pa350Tag())
+                {
+                    do_read_sizes(xml, tmp_width, tmp_height, ok);
+                    qDebug() << "pa350" << tmp_width.to_mm() << tmp_height.to_mm();
+                }
+            }
         }
         void do_read_params(Schema* schema, XmlStream& xml)
         {
             bool ok;
             while (xml.readNextStartElement())
             {
+                qDebug() << xml.name();
                 if (xml.name() == tags::diameterTag())
                 {
                     auto diam = do_read_units(xml, ok);
                     if (ok)
-                        qDebug() << diam.units();//schema->params()->set_diameter(diam);
+                        qDebug() << diam.to_mm();//schema->params()->set_diameter(diam);
                     else
                         xml.raiseError("error in diam tag: cant parse num");
                 }
@@ -46,8 +106,24 @@ namespace libschema
                 {
                     while (xml.readNextStartElement())
                     {
-
+                        qDebug() << xml.name();
+                        if (xml.name() == tags::pkaSawTag())
+                        {
+                            auto saw = do_read_units(xml, ok);
+                            qDebug() << ok << saw.to_mm();
+                        }
+                        else if (xml.name() == tags::dwsSawTag())
+                        {
+                            auto saw = do_read_units(xml, ok);
+                            qDebug() << ok << saw.to_mm();
+                        }
                     }
+                }
+                else if (xml.name() == tags::rotatorTag())
+                {
+                    auto attr = xml.attributes().value(tags::disabledAttr());
+                    qDebug() << attr;
+                    xml.skipCurrentElement();
                 }
             }
         }
@@ -55,6 +131,7 @@ namespace libschema
         {
             while (xml.readNextStartElement())
             {
+                qDebug() << xml.name();
                 if (xml.name() == tags::paramsTag())
                 {
                     do_read_params(schema, xml);
