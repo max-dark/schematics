400f70324f5f55eb423478435c80cb339dfb830a
Andreevo LDK <andreevo.ldk@yandex.ru>
2020-09-25 14:31:34 +0300

    add methods for SchemeCalculator

--- schematics/lib/libcoords/coords/coords.cxx
+++ schematics/lib/libcoords/coords/coords.cxx
@@ -2,9 +2,136 @@
 
 namespace Coords {
 
+// >>>>>>>>>>>>>>>>>
+
 void SchemeCalculator::calculate(const Schema *schema)
 {
     //
 }
+
+// <<<<<<<<<<<<<<<<<
+// >>>>>>>>>>>>>>>>>
+
+Unit SchemeCalculator::fbs1Width() const
+{
+    return fbs1_width;
+}
+
+Unit SchemeCalculator::fbs1Height() const
+{
+    return fbs1_height;
+}
+
+// <<<<<<<<<<<<<<<<<
+// >>>>>>>>>>>>>>>>>
+
+libschema::Unit SchemeCalculator::fbs2Align() const
+{
+    return fbs2_align;
+}
+
+libschema::Unit SchemeCalculator::fbs2Width() const
+{
+    return fbs2_width;
+}
+
+libschema::Unit SchemeCalculator::fbs2Height() const
+{
+    return fbs2_height;
+}
+
+// <<<<<<<<<<<<<<<<<
+// >>>>>>>>>>>>>>>>>
+
+libschema::Unit SchemeCalculator::p1Width() const
+{
+    return p1_width;
+}
+
+libschema::Unit SchemeCalculator::p1Height() const
+{
+    return p1_height;
+}
+
+libschema::Unit SchemeCalculator::p1Bottom() const
+{
+    return p1_bottom;
+}
+
+libschema::Unit SchemeCalculator::p1Top() const
+{
+    return p1_top;
+}
+
+// <<<<<<<<<<<<<<<<<
+// >>>>>>>>>>>>>>>>>
+
+libschema::Unit SchemeCalculator::p2Width() const
+{
+    return p2_width;
+}
+
+libschema::Unit SchemeCalculator::p2Height() const
+{
+    return p2_height;
+}
+
+libschema::Unit SchemeCalculator::p2Bottom() const
+{
+    return p2_bottom;
+}
+
+libschema::Unit SchemeCalculator::p2Top() const
+{
+    return p2_top;
+}
+
+Unit SchemeCalculator::p2OuterSaw() const
+{
+    return p2_outer_saw;
+}
+
+Unit SchemeCalculator::p2RollerPos() const
+{
+    return p2_roller_pos;
+}
+
+// <<<<<<<<<<<<<<<<<
+// >>>>>>>>>>>>>>>>>
+
+libschema::Unit SchemeCalculator::p3Width() const
+{
+    return p3_width;
+}
+
+libschema::Unit SchemeCalculator::p3Height() const
+{
+    return p3_height;
+}
+
+libschema::Unit SchemeCalculator::p3Bottom() const
+{
+    return p3_bottom;
+}
+
+libschema::Unit SchemeCalculator::p3Top() const
+{
+    return p3_top;
+}
+
+// <<<<<<<<<<<<<<<<<
+// >>>>>>>>>>>>>>>>>
+
+libschema::Unit SchemeCalculator::dwsWidth() const
+{
+    return dws_width;
+}
+
+libschema::Unit SchemeCalculator::dwsHeight() const
+{
+    return dws_height;
+}
+
+// <<<<<<<<<<<<<<<<<
 
 } // namespace CoordSystem
--- schematics/lib/libcoords/coords/coords.hxx
+++ schematics/lib/libcoords/coords/coords.hxx
@@ -17,5 +17,65 @@ namespace Coords
     public:
         /// calculate sizes and positions by scheme
         void calculate(const Schema* schema);
+
+        Unit fbs1Width() const;
+        Unit fbs1Height() const;
+
+        Unit fbs2Align() const;
+        Unit fbs2Width() const;
+        Unit fbs2Height() const;
+
+        Unit p1Width() const;
+        Unit p1Height() const;
+        Unit p1Bottom() const;
+        Unit p1Top() const;
+
+        Unit p2Width() const;
+        Unit p2Height() const;
+        Unit p2Bottom() const;
+        Unit p2Top() const;
+        Unit p2OuterSaw() const;
+
+        Unit p2RollerPos() const;
+
+        Unit p3Width() const;
+        Unit p3Height() const;
+        Unit p3Bottom() const;
+        Unit p3Top() const;
+
+        Unit dwsWidth() const;
+        Unit dwsHeight() const;
+
+    private:
+        // f1
+        Unit fbs1_width;
+        Unit fbs1_height;
+        // f2
+        Unit fbs2_width;
+        Unit fbs2_height;
+        Unit fbs2_align;
+        // p1
+        Unit p1_width;
+        Unit p1_height;
+        Unit p1_bottom;
+        Unit p1_top;
+        // p2
+        Unit p2_width;
+        Unit p2_height;
+        Unit p2_bottom;
+        Unit p2_top;
+        Unit p2_outer_saw;
+        Unit p2_roller_pos;
+        // p2 / rotator
+        Unit r2_width;
+        Unit r2_height;
+        // p3
+        Unit p3_width;
+        Unit p3_height;
+        Unit p3_bottom;
+        Unit p3_top;
+        // dws
+        Unit dws_height;
+        Unit dws_width;
     };
 } // namespace CoordSystem
