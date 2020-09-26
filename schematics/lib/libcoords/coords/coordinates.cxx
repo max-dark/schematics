0d9fa22be181048d67c8e852f27cfece11d78951
Max Dark <maxim.dark@gmail.com>
2020-09-26 20:47:05 +0300

    add coord calculation for FBS1/FBS2

--- schematics/lib/libcoords/coords/coordinates.cxx
+++ schematics/lib/libcoords/coords/coordinates.cxx
@@ -29,6 +29,42 @@ void Coordinates::fill_from(const libschema::Schema *schema)
 {
     Geometry g;
     g.calculate(schema);
+
+    const auto two = Unit::from_units(2);
+
+    // FBS1
+    {
+        auto height = g.fbs1Height();
+        auto x_half = g.fbs1Width() / two;
+
+        setById(POS_ID_FBS1_PRESS1_HEIGHT, height);
+        setById(POS_ID_FBS1_PRESS2_HEIGHT, height);
+
+        setById(POS_ID_FBS1_LEFT_BLOCK, x_half);
+        setById(POS_ID_FBS1_RIGHT_BLOCK, x_half);
+
+        setById(POS_ID_FBS1_ROTATOR_HEIGHT, height);
+        setById(POS_ID_FBS1_ROTATOR_INP_WIDTH, height);
+        setById(POS_ID_FBS1_ROTATOR_OUT_WIDTH, height);
+    }
+
+    // FBS2
+    {
+        auto height = g.fbs2Height();
+        auto x_half = g.fbs2Width() / two;
+        auto align  = g.fbs2Align();
+
+        setById(POS_ID_FBS2_ALIGN1, align);
+        setById(POS_ID_FBS2_ALIGN2, align);
+        setById(POS_ID_FBS2_ALIGN3, align);
+
+        setById(POS_ID_FBS2_PRESS1_HEIGHT, height);
+        setById(POS_ID_FBS2_PRESS2_HEIGHT, height);
+        setById(POS_ID_FBS2_PRESS3_HEIGHT, height);
+
+        setById(POS_ID_FBS2_LEFT_BLOCK, x_half);
+        setById(POS_ID_FBS2_RIGHT_BLOCK, x_half);
+    }
 }
 
 } // namespace Coords
