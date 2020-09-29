3dd2d4fa036a9dba704d049e5c4479c32033b212
Andreevo LDK <andreevo.ldk@yandex.ru>
2020-09-29 15:36:24 +0300

    add debug code for offsets

--- schematics/src/main.cxx
+++ schematics/src/main.cxx
@@ -5,6 +5,18 @@
 
 #include <QtDebug>
 
+#include <iostream>
+struct Printer: public Coords::OffsetVisitor
+{
+    void visit(const Coords::UnitOffset& o) override {
+        std::clog << "Unit " << o.offset;
+    }
+    void visit(const Coords::DigitOffset& o) override
+    {
+        std::clog << "Digit " << o.offset << ' ' << o.per_mm;
+    }
+};
+
 int main(int argc, char** argv)
 {
     QApplication app{argc, argv};
@@ -16,6 +28,18 @@ int main(int argc, char** argv)
                           "schematics.db");
     // init database service
     svc->startStorage();
+    {
+        Printer printer;
+        auto off = svc->offsets();
+        auto l = off->offsets();
+        for (auto const &p: l)
+        {
+            std::clog << p.first << ' ';
+            p.second->accept(printer);
+            std::clog << std::endl;
+        }
+        off->clear(l);
+    }
     // connect to main PLC
     {
         svc->startSawPlc();
