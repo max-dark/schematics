5cab973920697065d09b9ddd2f40a295a0f80fe0
Andreevo LDK <andreevo.ldk@yandex.ru>
2020-09-29 08:53:32 +0300

    add more code for db service

--- schematics/src/services/database.cxx
+++ schematics/src/services/database.cxx
@@ -3,6 +3,7 @@
 #include <QSqlDatabase>
 #include <QSqlQuery>
 #include <QVariant>
+#include <QSet>
 
 namespace Schematics::Service
 {
@@ -30,15 +31,20 @@ bool Database::checkStructure()
     auto ok = db.isValid() && db.isOpen();
     if (ok)
     {
-        auto tables = db.tables();
+        auto const table_list = db.tables();
+        auto const tables = QSet(table_list.begin(), table_list.end());
+        auto table_exists = [&tables](const QString& name)
+        {
+            return tables.end() != tables.find(name);
+        };
         ok = ok && (! tables.empty());
-        ok = ok && (tables.indexOf("config") != -1);
-        ok = ok && (tables.indexOf("offsets") != -1);
-        ok = ok && (tables.indexOf("speeds") != -1);
-        ok = ok && (tables.indexOf("delays") != -1);
-        ok = ok && (tables.indexOf("alarms") != -1);
-        ok = ok && (tables.indexOf("sensors") != -1);
-        ok = ok && (tables.indexOf("motors") != -1);
+        ok = ok && table_exists("config");
+        ok = ok && table_exists("offsets");
+        ok = ok && table_exists("speeds");
+        ok = ok && table_exists("delays");
+        ok = ok && table_exists("alarms");
+        ok = ok && table_exists("sensors");
+        ok = ok && table_exists("motors");
     }
     return ok;
 }
@@ -47,7 +53,9 @@ bool Database::getValueByName(const QString &name, QString &value)
 {
     QSqlQuery get_value;
 
-    auto ok = get_value.prepare("select value from config where name = :var_name");
+    auto ok = get_value.prepare(
+        "select value from config where name = :var_name"
+        );
     if (ok)
     {
         get_value.bindValue(":var_name", name);
@@ -61,8 +69,20 @@ bool Database::getValueByName(const QString &name, QString &value)
     return ok;
 }
 
-bool Database::setValueByName(const QString &name, const QString &value) {
-    return false;
+bool Database::setValueByName(const QString &name, const QString &value)
+{
+    QSqlQuery set_query;
+
+    auto ok = set_query.prepare(
+        "update config set value = :value where name = :name"
+        );
+    if (ok)
+    {
+        set_query.bindValue(":name", name);
+        set_query.bindValue(":value", value);
+        ok = set_query.exec();
+    }
+    return ok;
 }
 
 } // namespace Schematics::Service
