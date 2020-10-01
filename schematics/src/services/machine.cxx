bcb165ca623ce10030e41ecd5f5ac47c4df6b38e
Andreevo LDK <andreevo.ldk@yandex.ru>
2020-10-01 11:22:14 +0300

    add 'Tags' types

--- schematics/src/services/machine.cxx
+++ schematics/src/services/machine.cxx
@@ -2,10 +2,64 @@
 
 #include <s7_micro_client.h>
 
+#include <cstdint>
+#include <type_traits>
+#ifdef WIN32
+#include <WinSock2.h>
+#else
+#include <arpa/inet.h>
+#endif
+
 char * ErrCliText(int Error, char* Result, int TextLen);
 
 namespace Schematics::Service
 {
+
+namespace tool {
+template<class T>
+T to_plc(T v)
+{
+    static_assert(std::is_integral_v<T>, "T must be a number");
+    static_assert(sizeof (T) == 2 || sizeof (T) == 4, "sizeof T");
+    void * ptr = &v;
+
+    if constexpr(sizeof(T) == 2)
+    {
+        auto &ref = *static_cast<uint16_t*>(ptr);
+        ref = htons(ref);
+    }
+
+    if constexpr(sizeof(T) == 4)
+    {
+        auto &ref = *static_cast<uint32_t*>(ptr);
+        ref = htonl(ref);
+    }
+
+    return v;
+}
+
+template<class T>
+T from_plc(T v)
+{
+    static_assert(std::is_integral_v<T>, "T must be a number");
+    static_assert(sizeof (T) == 2 || sizeof (T) == 4, "sizeof T");
+    void * ptr = &v;
+
+    if constexpr(sizeof(T) == 2)
+    {
+        auto &ref = *static_cast<uint16_t*>(ptr);
+        ref = ntohs(ref);
+    }
+
+    if constexpr(sizeof(T) == 4)
+    {
+        auto &ref = *static_cast<uint32_t*>(ptr);
+        ref = ntohl(ref);
+    }
+
+    return v;
+}
+} // tool
 
 struct Machine::Connection: protected TSnap7MicroClient
 {
@@ -33,10 +87,10 @@ struct Machine::Connection: protected TSnap7MicroClient
      * @return
      */
     bool write(int area, int block_no, int start,
-               int count, int type, void* data)
+               int count, int type, const void* data)
     {
         return ok(WriteArea(
-            area, block_no, start, count, type, data
+            area, block_no, start, count, type, const_cast<void*>(data)
             ));
     }
 
@@ -61,9 +115,9 @@ struct Machine::Connection: protected TSnap7MicroClient
     {
         return ok(ReadMultiVars(data, count));
     }
-    bool writeMany(PS7DataItem data, int count)
+    bool writeMany(const TS7DataItem* data, int count)
     {
-        return ok(WriteMultiVars(data, count));
+        return ok(WriteMultiVars(const_cast<PS7DataItem>(data), count));
     }
 
     int lastError() const { return last_op; }
@@ -115,6 +169,30 @@ void Machine::disconnect() const
     self()->disconnect();
 }
 
+bool Machine::readTag(Tag &tag) const
+{
+    return self()->read(
+        tag.areaCode(),
+        tag.blockNo(),
+        tag.address(),
+        tag.itemCount(),
+        tag.typeCode(),
+        tag.pointer()
+                );
+}
+
+bool Machine::writeTag(const Tag &tag) const
+{
+    return self()->write(
+        tag.areaCode(),
+        tag.blockNo(),
+        tag.address(),
+        tag.itemCount(),
+        tag.typeCode(),
+        tag.pointer()
+        );
+}
+
 bool Machine::hasError() const
 {
     return self()->hasError();
@@ -134,4 +212,108 @@ Machine::Connection *Machine::self() const
 {
     return selfptr;
 }
+
+int Tag::areaCode() const
+{
+    switch (area) {
+    case Area::DATA  : return S7AreaDB;
+    case Area::INPUT : return S7AreaPE;
+    case Area::MEMORY: return S7AreaMK;
+    case Area::OUTPUT: return S7AreaPA;
+    }
+    return 0;
+}
+
+int Tag::typeCode() const
+{
+    switch (type) {
+    case Type::BIT: return S7WLBit;
+    case Type::BYTE: return S7WLByte;
+    case Type::INT: return S7WLInt;
+    case Type::WORD: return S7WLWord;
+    case Type::DINT: return S7WLDInt;
+    case Type::DWORD: return S7WLDWord;
+    }
+    return 0;
+}
+
+int Tag::address() const
+{
+    return start;
+}
+
+int Tag::blockNo() const
+{
+    return block;
+}
+
+int Tag::itemCount() const
+{
+    return count;
+}
+
+Tag::~Tag() = default;
+
+BoolTag::BoolTag(Tag::Area area_id,
+                 int byte_no, int bit_no, int block_no)
+    : Tag{}
+{
+    area = area_id;
+    type = Type::BIT;
+    start = (byte_no << 3) | bit_no;
+    block = block_no;
+    value = false;
+}
+
+ByteTag::ByteTag(Tag::Area area_id,
+                 int byte_no, int block_no)
+    : Tag{}
+{
+    area = area_id;
+    type = Type::BYTE;
+    start = (byte_no << 3);
+    block = block_no;
+    value = 0;
+}
+
+IntTag::IntTag(Tag::Area area_id, int byte_no, int block_no)
+    : Tag{}
+{
+    area = area_id;
+    type = Type::INT;
+    start = (byte_no << 3);
+    block = block_no;
+    value = 0;
+}
+
+void IntTag::set(Tag::Int v)
+{
+    value = tool::to_plc(v);
+}
+
+Tag::Int IntTag::get() const
+{
+    return tool::from_plc(value);
+}
+
+DIntTag::DIntTag(Tag::Area area_id, int byte_no, int block_no)
+    : Tag{}
+{
+    area = area_id;
+    type = Type::DINT;
+    start = (byte_no << 3);
+    block = block_no;
+    value = 0;
+}
+
+void DIntTag::set(Tag::DInt v)
+{
+    value = tool::to_plc(v);
+}
+
+Tag::DInt DIntTag::get() const
+{
+    return tool::from_plc(value);
+}
+
 } // namespace Schematics::Service
--- schematics/src/services/machine.hxx
+++ schematics/src/services/machine.hxx
@@ -3,8 +3,92 @@
 
 #include <QObject>
 
+#include <cstdint>
+
 namespace Schematics::Service
 {
+
+struct Tag
+{
+    using Byte = std::uint8_t;
+    using Word = std::uint16_t;
+    using DWord = std::uint32_t;
+    using Int = std::int16_t;
+    using DInt = std::int32_t;
+    enum class Area
+    {
+        INPUT, OUTPUT, MEMORY, DATA
+    };
+    enum class Type
+    {
+        BIT, BYTE, WORD, DWORD, INT, DINT
+    };
+    int areaCode() const;
+    int typeCode() const;
+    int address() const;
+    int blockNo() const;
+    int itemCount() const;
+    virtual const void* pointer() const = 0;
+    virtual void* pointer() = 0;
+protected:
+    virtual ~Tag();
+    Area area = Area::MEMORY;
+    Type type = Type::BIT;
+    int start = 0;
+    int count = 1;
+    int block = 0;
+};
+
+struct BoolTag: public Tag
+{
+    BoolTag(Area area_id,
+            int byte_no, int bit_no, int block_no = 0);
+    const void* pointer() const override { return &value; }
+    void* pointer() override { return &value; }
+    void set(bool v) { value = v; }
+    bool get() const { return value; }
+    void invert() { set(!get()); }
+private:
+    bool value;
+};
+
+
+struct ByteTag: public Tag
+{
+    ByteTag(Area area_id,
+            int byte_no, int block_no = 0);
+    const void* pointer() const override { return &value; }
+    void* pointer() override { return &value; }
+    void set(Byte v) { value = v; }
+    Byte get() const { return value; }
+private:
+    Byte value;
+};
+
+
+struct IntTag: public Tag
+{
+    IntTag(Area area_id,
+            int byte_no, int block_no = 0);
+    const void* pointer() const override { return &value; }
+    void* pointer() override { return &value; }
+    void set(Int v);
+    Int get() const;
+private:
+    Int value;
+};
+
+struct DIntTag: public Tag
+{
+    DIntTag(Area area_id,
+           int byte_no, int block_no = 0);
+    const void* pointer() const override { return &value; }
+    void* pointer() override { return &value; }
+    void set(DInt v);
+    DInt get() const;
+private:
+    DInt value;
+};
 
 class Machine: public QObject
 {
@@ -17,6 +101,10 @@ public:
     bool connect(const QString& address, int rack, int slot) const;
     bool connected() const;
     void disconnect() const;
+
+    bool readTag(Tag& tag) const;
+    bool writeTag(const Tag& tag) const;
+
     bool hasError() const;
     int lastError() const;
     QString errorMessage() const;
