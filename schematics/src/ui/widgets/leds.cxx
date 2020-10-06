f878500e2b872597b3305e14fa2517fd072ee2bd
Andreevo LDK <andreevo.ldk@yandex.ru>
2020-10-06 13:22:22 +0300

    add Led indicator widgets

---
+++ schematics/src/ui/widgets/leds.cxx
@@ -0,0 +1,48 @@
+#include "leds.hxx"
+
+#include <vector>
+
+namespace Schematics::Ui::Widgets
+{
+
+QPixmap &Led::getPixmap(Led::Color color)
+{
+    static std::vector<QPixmap> cache;
+
+    if (cache.empty())
+    {
+        Q_INIT_RESOURCE(ledpix);
+        cache.emplace_back(":/led/gray.png");
+        cache.emplace_back(":/led/green.png");
+        cache.emplace_back(":/led/yellow.png");
+        cache.emplace_back(":/led/orange.png");
+        cache.emplace_back(":/led/red.png");
+    }
+
+    return cache[color];
+}
+
+Led::Led(QWidget *parent)
+    : QLabel(parent)
+{
+    setFixedSize(21, 21);
+    applyColor(GRAY);
+}
+
+void Led::setColor(Led::Color color)
+{
+    if (current != color)
+    {
+        applyColor(color);
+    }
+}
+
+void Led::applyColor(Led::Color color)
+{
+    current = color;
+    setPixmap(getPixmap(color));
+}
+
+Led::~Led() = default;
+
+} // namespace Schematics::Ui::Widgets
