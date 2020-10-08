f8933ce0d92f07faab93f79473e649348d34d225
Max Dark <maxim.dark@gmail.com>
2020-10-08 23:44:38 +0300

    add inital code for LedList widget

--- schematics/src/ui/widgets/ledlist.cxx
+++ schematics/src/ui/widgets/ledlist.cxx
@@ -1,11 +1,70 @@
 #include "ledlist.hxx"
 
+#include <QLayout>
+
 namespace Schematics::Ui::Widgets
 {
 
 LedList::LedList(QWidget *parent)
         : QScrollArea(parent)
-{}
+{
+    setWidget(new QWidget);
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+}
+
+Led::Color LedList::selectColor(bool state)
+{
+    return state /* is on */ ? color_on : color_off;
+}
+
+void LedList::setColors(Led::Color on, Led::Color off)
+{
+    color_on = on;
+    color_off = off;
+}
+
+void LedList::updateState(const LedList::StateMap& state)
+{
+    for(const auto &led_id: leds.keys())
+    {
+        if (auto s = state.find(led_id); s != state.end())
+        {
+            auto new_state = s.value();
+            auto new_color = selectColor(new_state);
+            leds[led_id]->setColor(new_color);
+        }
+    }
+}
+
+void LedList::createLeds(const LedList::LabelMap &labels)
+{
+    auto owner = widget();
+    auto old_box = owner->layout();
+
+    // remove all widgets
+    delete old_box;
+    leds.clear();
+
+    // create new
+    auto box = new QGridLayout;
+    auto col = 0;
+    auto row = 0;
+    auto half = labels.size() / 2 + (labels.size() % 2);
+    for (auto id : labels.keys())
+    {
+        auto label = new QLabel{labels[id]};
+        auto led = new Led{};
+        box->addWidget(led, row, col);
+        box->addWidget(label, row, col + 1);
+
+        ++row;
+        if (row == half)
+        {
+            row = 0; col += 2;
+        }
+    }
+    owner->setLayout(box);
+}
 
 LedList::~LedList() = default;
 } // namespace Schematics::Ui::Widgets
--- schematics/src/ui/widgets/ledlist.hxx
+++ schematics/src/ui/widgets/ledlist.hxx
@@ -5,6 +5,8 @@
 
 #include <ui/widgets/leds.hxx>
 
+#include <QMap>
+
 namespace Schematics::Ui::Widgets
 {
 
@@ -12,8 +14,21 @@ class LedList: public QScrollArea
 {
     Q_OBJECT
 public:
+    using StateMap = QMap<int, bool>;
+    using LedMap = QMap<int, Led*>;
+    using LabelMap = QMap<int, QString>;
     explicit LedList(QWidget* parent = nullptr);
     ~LedList() override;
+
+    void createLeds(const LabelMap& labels);
+    void updateState(const StateMap& state);
+    void setColors(Led::Color on, Led::Color off);
+private:
+    Led::Color selectColor(bool state);
+    Led::Color color_on = Led::Color::GREEN;
+    Led::Color color_off = Led::Color::RED;
+
+    LedMap leds;
 };
 
 } // namespace Schematics::Ui::Widgets
